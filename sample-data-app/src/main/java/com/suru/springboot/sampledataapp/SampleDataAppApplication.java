07c52737ffca0e470947e6bcbff6564a579a0f65
SuRu <su33.dev@gmail.com>
2017-09-22 09:53:13 -0500

    initial project setup

---
+++ sample-data-app/src/main/java/com/suru/springboot/sampledataapp/SampleDataAppApplication.java
@@ -0,0 +1,12 @@
+package com.suru.springboot.sampledataapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDataAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleDataAppApplication.class, args);
+	}
+}
